dbaa0c1f6a071e878915b9ef6947e989f5f37d59
Richard Bailey <richard@impactexpress.co.uk>
2020-02-13 15:37:00 +0000

    Updating registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.customer.personal.master')
 
 @section('content')
-<div class="container">
+<div class="container bg-yellow">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
