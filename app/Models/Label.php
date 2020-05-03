56eafb9cfa981869ccfc7b2d2ec3d1ac0c44b0fa
Richard Bailey <richard@richardbailey.xyz>
2020-05-03 15:50:45 +0000

    label barcode details

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -10,6 +10,20 @@ class Label extends Model
         'user_id',
         'shipment_id',
         'carrier',
-        'image'
+        'image',
+        'barcode_carrier_id',
+        'barcode_carrier_name',
+        'carrier_logo_ref',
+        'delivery_method_desc',
+        'delivery_method_code',
+        'barcode_number',
+        'barcode_length',
+        'barcode_symbology',
+        'barcode_display',
+        'sort_level_1',
+        'sort_level_2',
+        'sort_level_3',
+        'sort_level_4',
+        'sort_level_5',
     ];
 }
--- database/migrations/2020_03_11_121144_create_labels_table.php
+++ database/migrations/2020_03_11_121144_create_labels_table.php
@@ -19,6 +19,20 @@ class CreateLabelsTable extends Migration
             $table->integer('shipment_id');
             $table->string('carrier');
             $table->text('image');
+            $table->string('barcode_carrier_id');
+            $table->string('barcode_carrier_name');
+            $table->string('carrier_logo_ref');
+            $table->string('delivery_method_desc');
+            $table->string('delivery_method_code');
+            $table->string('barcode_number');
+            $table->string('barcode_length');
+            $table->string('barcode_symbology');
+            $table->string('barcode_display');
+            $table->string('sort_level_1');
+            $table->string('sort_level_2');
+            $table->string('sort_level_3');
+            $table->string('sort_level_4');
+            $table->string('sort_level_5');
             $table->timestamps();
         });
     }
