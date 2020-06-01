2048e35e154b7a2bbf6039dd3673a4e8bdc2c8e3
Richard Bailey <richard@richardbailey.xyz>
2020-06-01 14:17:33 +0000

    multiple modals

--- public/js/modal.js
+++ public/js/modal.js
@@ -93,7 +93,40 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log($);
+// Get the modal
+// var modal = document.getElementById("myModal");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+var modalBtns = $('.modalBtn');
+modalBtns.on('click', function () {
+  var ref = $(this).data("ref");
+  var modal = $('#modal-' + ref);
+  modal.css("display", "block");
+  $('#close-' + ref).on('click', function () {
+    modal.css("display", "none");
+  });
+  $(window).on('click', function (e) {
+    if (e.target.id == 'modal-' + ref) {
+      modal.css("display", "none");
+    }
+  });
+});
 
 /***/ }),
 
