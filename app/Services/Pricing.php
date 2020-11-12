004023fa7604dd734fe3f221943a89bb1ce5f497
Richard Bailey <richard@richardbailey.xyz>
2020-11-12 15:58:00 +0000

    priceing charges

--- app/Services/Pricing.php
+++ app/Services/Pricing.php
@@ -3,15 +3,12 @@
 namespace App\Services;
 
 use App\Models\Tariff;
-use App\Services\Weighting;
+use App\Models\Surcharge;
 
 class Pricing
 {
-
-    protected static $hermesBackToImpact = 4.20;
-
     public static function getPrice(array $dims, float $parcelWeight, int $destinationCountryZone) : float {
-        
+
         $applicableWeight = Weighting::calculateApplicableWeight($dims['length'], $dims['width'], $dims['height'], $parcelWeight);
 
         $tariffPrice = Tariff::where([
@@ -21,15 +18,28 @@ class Pricing
         ])->first()->amount;
 
         $price = $tariffPrice; // * $quantity;
-        
-        $price = $price + self::getHermesBackToImpactPrice();
 
-        // dd(money_format('%n', $price));
+        // DHL COVID Surcharge: amount per kg
+        $price += $parcelWeight * self::getDHLCOVIDSurcharge();
+
+        // Fuel surcharge: plus a percentage
+        $price += $price * (self::getFuelSurcharge() / 100);
+
+        // Hermes pickup charge: flat rate
+        $price = $price + self::getHermesPickupCharge();
 
         return $price;
     }
 
-    protected static function getHermesBackToImpactPrice() : float {
-        return self::$hermesBackToImpact;
+    protected static function getHermesPickupCharge() : float {
+        return Surcharge::where('name', 'hermesPickupCharge')->first()->value;
+    }
+
+    protected static function getFuelSurcharge() : float {
+        return Surcharge::where('name', 'fuelSurcharge')->first()->value;
+    }
+
+    protected static function getDHLCOVIDSurcharge() : float {
+        return Surcharge::where('name', 'dhlCovidCharge')->first()->value;
     }
 }
